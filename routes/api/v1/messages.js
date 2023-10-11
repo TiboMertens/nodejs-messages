38084dc30fbce2eef43005fded0c1efdd23be0e0
TiboMertens <tibomertens25@gmail.com>
2023-10-11 20:39:13 +0200

    feat(lab4): can GET id & username

--- routes/api/v1/messages.js
+++ routes/api/v1/messages.js
@@ -4,13 +4,6 @@ const express = require('express');
 //create router
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.json({
-        status: "success",
-        message: "GET messages",
-    })
-});
-
 router.post("/", (req, res) => {
     let message = req.body.message;
 
@@ -18,6 +11,28 @@ router.post("/", (req, res) => {
         status: "success",
         message: `POST ${message}`,
     });
+});
+
+router.get("/", (req, res) => {
+    const username = req.query.user;
+    const id = req.query.id;
+
+    if (username) {
+        res.json({
+            status: "success",
+            message: `GET messages with username ${username}`,
+        });
+    } else if (id) {
+        res.json({
+            status: "success",
+            message: `GET messages with id ${id}`,
+        });
+    } else {
+        res.json({
+            status: "success",
+            message: "GET all messages",
+        });
+    }
 });
 
 module.exports = router;
