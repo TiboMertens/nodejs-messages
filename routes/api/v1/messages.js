0b35cb31ecfad22d1bedf6aed5934006d9b8c9c9
TiboMertens <tibomertens25@gmail.com>
2023-10-12 15:44:37 +0200

    style: format document

--- routes/api/v1/messages.js
+++ routes/api/v1/messages.js
@@ -1,62 +1,62 @@
 //require express
-const express = require('express');
+const express = require("express");
 
 //create router
 const router = express.Router();
 
 router.post("/", (req, res) => {
-    let message = req.body.message;
-    let user = req.body.user;
+  let message = req.body.message;
+  let user = req.body.user;
 
-    res.json({
-        status: "success",
-        message: `POST ${message} for user ${user}`,
-    });
+  res.json({
+    status: "success",
+    message: `POST ${message} for user ${user}`,
+  });
 });
 
 router.put("/:id", (req, res) => {
-    const id = req.params.id;
-    const message = req.body.message;
+  const id = req.params.id;
+  const message = req.body.message;
 
-    res.json({
-        status: "success",
-        message: `PUT ${message} with id ${id}`,
-    });
+  res.json({
+    status: "success",
+    message: `PUT ${message} with id ${id}`,
+  });
 });
 
 router.delete("/:id", (req, res) => {
-    let id = req.params.id;
+  let id = req.params.id;
 
-    res.json({
-        status: "success",
-        message: `DELETE message with id ${id}`,
-    });
+  res.json({
+    status: "success",
+    message: `DELETE message with id ${id}`,
+  });
 });
 
 router.get("/", (req, res) => {
-    const username = req.query.user;
-    const id = req.query.id;
+  const username = req.query.user;
+  const id = req.query.id;
 
-    if (username) {
-        res.json({
-            status: "success",
-            message: `GET messages with username ${username}`,
-        });
-    } else {
-        res.json({
-            status: "success",
-            message: "GET all messages",
-        });
-    }
+  if (username) {
+    res.json({
+      status: "success",
+      message: `GET messages with username ${username}`,
+    });
+  } else {
+    res.json({
+      status: "success",
+      message: "GET all messages",
+    });
+  }
 });
 
 router.get("/:id", (req, res) => {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    res.json({
-        status: "success",
-        message: `GET message with id ${id}`,
-    });
+  res.json({
+    status: "success",
+    message: `GET message with id ${id}`,
+  });
 });
 
 module.exports = router;
