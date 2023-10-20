5f02d7ce419d76d205656ec53ad945508d05eda2
TiboMertens <tibomertens25@gmail.com>
2023-10-20 20:44:19 +0200

    feat: use mongodb for POST and GET request

--- routes/api/v1/messages.js
+++ routes/api/v1/messages.js
@@ -1,16 +1,38 @@
 //require express
 const express = require("express");
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+const messageSchema = new schema({
+  id: Number,
+  user: String,
+  message: String,
+});
+const Message = mongoose.model("Message", messageSchema);
 
 //create router
 const router = express.Router();
 
-router.post("/", (req, res) => {
-  let user = req.body.message.user;
+router.post("/", async (req, res) => {
+  // Create a new instance of the Message model
+  const message = new Message();
+  message.message = req.body.message.text;
+  message.user = req.body.message.user;
 
-  res.json({
-    status: "success",
-    message: `POSTING a new message for user ${user}`,
-  });
+  try {
+    // Save the message using async/await
+    const savedMessage = await message.save();
+    res.json({
+      status: "success",
+      message: `POSTING a new message for user ${message.user}`,
+      data: savedMessage,
+    });
+  } catch (err) {
+    // Handle any errors
+    console.error(err);
+    res
+      .status(500)
+      .json({ status: "error", message: "Error saving the message" });
+  }
 });
 
 router.put("/:id", (req, res) => {
@@ -32,20 +54,31 @@ router.delete("/:id", (req, res) => {
   });
 });
 
-router.get("/", (req, res) => {
-  const username = req.query.user;
-  const id = req.query.id;
-
-  if (username) {
-    res.json({
-      status: "success",
-      message: `GET messages with username ${username}`,
-    });
-  } else {
-    res.json({
-      status: "success",
-      message: "GET all messages",
-    });
+router.get("/", async (req, res) => {
+  try {
+    // Find all messages with username in the url, otherwise return all messages
+    const username = req.query.user;
+    if (username) {
+      const messages = await Message.find({ user: username });
+      res.json({
+        status: "success",
+        message: `GET all messages with username ${username}`,
+        data: messages,
+      });
+    } else {
+      const messages = await Message.find();
+      res.json({
+        status: "success",
+        message: "GET all messages",
+        data: messages,
+      });
+    }
+  } catch (err) {
+    // Handle any errors
+    console.error(err);
+    res
+      .status(500)
+      .json({ status: "error", message: "Error getting messages" });
   }
 });
 
